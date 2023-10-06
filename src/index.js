0dc80da9807ae8f70d6d1474624c09eaa755e5e1
Tech-goodat <felixkiprotich2000@gmail.com>
2023-10-06 15:15:55 +0300

    done with assighnment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
+fetch ('http://localhost:3000/dogs')
+.then(res => res.json())
+
+.then(dogs => {
+    dogs.forEach(dog => {
+        renderDog(dog)
+    })
+
 
 })
+
+function renderDog(dog){
+
+    dogTable = document.querySelector('#table-body')
+    dogRow = document.createElement('tr')
+    dogRow.innerHTML = `
+    <td>${dog.name}</td>
+    <td>${dog.breed}</td>`
+}
+})
+
+
